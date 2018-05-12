93237d2eae438c397249b5f9a0b53bcfb13cfdb8
travis <t3patterson@gmail.com>
2018-05-12 16:36:29 -0500

    cleaning up html comments

--- js/02-radio-check-inputs.js
+++ js/02-radio-check-inputs.js
@@ -1,2 +1,11 @@
 // .onChange function to modify another element
 //--------------------------------------------
+
+
+// I-a : Listen to 'change' event on checkbox (use specific selector),
+//       put result on DOM
+//--------------------------------------------
+
+
+// II-b : Listen to change event on
+//--------------------------------------------
